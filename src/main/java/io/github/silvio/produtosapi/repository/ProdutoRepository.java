854decca60c283431e108d84c5da513d9e80d3a2
silviocoutinho <silviocoutinho@ymail.com>
2025-04-22 22:08:39 -0300

    feat: metodo para retorna por nome

---
+++ src/main/java/io/github/silvio/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package io.github.silvio.produtosapi.repository;
+
+import io.github.silvio.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
